cb17ff63ece8b29bee4827b27514af590efe323f
Hyeongdong Kim <gudehdu10@gmail.com>
2022-02-16 19:55:18 +0900

    reactive web design add

--- frontend/client_management_system/src/setupProxy.js
+++ frontend/client_management_system/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://was:8080',
-            // target: 'http://localhost:8080',
+            // target: 'http://was:8080',
+            target: 'http://localhost:8080',
             changeOrigin: true
         })
     )
